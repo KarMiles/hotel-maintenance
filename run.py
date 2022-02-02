ddf101b3208c632dc40e78847d9b16e843709e9a
Karol Mileszko <karol.miles@gmail.com>
2022-02-02 07:25:35 +0000

    Create get_room_number function to get room number from user

--- run.py
+++ run.py
@@ -13,6 +13,18 @@ GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('hotel_maintenance')
 
 # check if connections are working:
-tickets = SHEET.worksheet('tickets')
-data = tickets.get_all_values()
-print(data)
+# tickets = SHEET.worksheet('tickets')
+# data = tickets.get_all_values()
+# print(data)
+
+def get_room_number():
+    """
+    Get room number from user.
+    """
+    print("Please enter room number.")
+    print("Room number should be 3 digits, e.g. 102.\n")
+    room_number = input("Room number here: ")
+
+    print(f"You entered room number {room_number}.")
+
+get_room_number()
